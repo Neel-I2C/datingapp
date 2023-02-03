276b48a3234685d0b5dff68e3b3e92075ce48c86
Neel-I2C <nm.idea2code@gmail.com>
2023-02-03 09:19:18 +0530

    03 February 2023

--- lib/screens/auth/login_with_phone/controller/enable_location_screen_controller.dart
+++ lib/screens/auth/login_with_phone/controller/enable_location_screen_controller.dart
@@ -1,12 +1,10 @@
 import 'dart:developer';
-
-import 'package:datingapp/utiles/networkservice/profile_create_service.dart';
 import 'package:get/get.dart';
 
 class EnableLocationScreenController extends GetxController {
   createProfile() async {
     try {
-      var profile = await ProfileCreateService.profileCreateService();
+      // var profile = await ProfileCreateService.profileCreateService();
     } catch (e) {
       log("CREATE PROFILE ERROR :: $e");
     }
