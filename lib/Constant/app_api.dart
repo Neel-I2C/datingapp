07b32d03b51d790123635a038106b3873c2d78b1
Neel-I2C <nm.idea2code@gmail.com>
2023-02-07 09:33:20 +0530

    07 February 2023

--- lib/Constant/app_api.dart
+++ lib/Constant/app_api.dart
@@ -1,6 +1,6 @@
 class AppApi {
   // static String baseUrl = "https://staging.sabbour.net/api/v1/";
-  static String baseUrl = "http://192.168.1.15:8000/";
+  static String baseUrl = "http://192.168.1.23:8000/";
 
   static String userRegister = "${baseUrl}user/";
 
